39ed4130f4f349d2c0d8288708951ebf89739428
Vistor25 <victorvoronko@gmail.com>
2017-12-04 13:21:21 +0300

    fixing

--- XMLTests/Tests.cs
+++ XMLTests/Tests.cs
@@ -17,59 +17,47 @@ namespace XMLTests
 
         [TestCase(null, typeof(ArgumentException))]
         [TestCase("", typeof(ArgumentException))]
-        public void CreateRepositoryWithoutPath_Exception(string path, Type exception)
+        public void CreateRepositoryWithEmptyPath(string path, Type exception)
         {
            Assert.Throws(exception, () => new XMLRepository(path));
         }
 
-        [TestCase("test.xml", typeof(IOException))]
-        public void GetStreamToRepository_RepositoryNotExsist_Exception(string repositoryName, Type exception)
+        [TestCase("aaaaa.xml", typeof(IOException))]
+        public void NotExistingPathExeption(string repositoryName, Type exception)
         {
             XMLRepository repository = new XMLRepository(repositoryName);
             Assert.Throws(exception, () => repository.Load());
         }
-
-        //Should be created repository
-        [TestCase("C:/Users/Viktar_Varanko/Source/Repos/Module06.BasicXML/XMLTests/test.xml")]
-        public void GetStreamToRepository_ReturnStreamToRepository(string path)
-        {
-            XMLRepository repository = new XMLRepository(path);
-            Stream stream = repository.Load();
-            Assert.AreNotEqual(stream, null);
-        }
-
-        //Should be created repository
+        
         [TestCase("C:/Users/Viktar_Varanko/Source/Repos/Module06.BasicXML/XMLTests/test.xml", null, typeof(ArgumentNullException))]
-        public void SaveDataInRepository_Exception(string path, Catalog catalog, Type exception)
+        public void SaveDataExeption(string path, Catalog catalog, Type exception)
         {
             XMLRepository repository = new XMLRepository(path);
             Assert.Throws(exception, () => repository.Save(catalog));
         }
         [TestCase(null, null, typeof(IOException))]
-        public void CreatingService_RepositoryNotFound_Exception(string filePath, Catalog catalog, Type exception)
+        public void CreateServiceWithNullRepository(string filePath, Catalog catalog, Type exception)
         {
             Assert.Throws(exception, () => new XMLService());
         }
 
-        //Should be created repository
         [TestCase("C:/Users/Viktar_Varanko/Source/Repos/Module06.BasicXML/XMLTests/test.xml")]
-        public void AddNewValueToRepository_SuccessAddedNewItem(string repositoryName)
+        public void AddNewEntity(string repositoryName)
         {
             XMLService service = new XMLService(repositoryName);
             bool operationResult = service.AddEntity(new Book() {
                 Id = 365461,
                 Title = "New Book",
                 City = "Minsk",
-                Annotation = "aaaaaaa",
+                Annotation = "aaaaddhdhaaa",
                 Authors = new List<string> { "Victor" },
                 Publisher = "EPAM",
                 Description = "Book" });
             Assert.AreEqual(operationResult, true);
         }
 
-        //Should be created repository
         [TestCase("C:/Users/Viktar_Varanko/Source/Repos/Module06.BasicXML/XMLTests/test.xml", typeof(ArgumentNullException))]
-        public void AddNewValueToRepository_FailedAddedNewItem(string repositoryName, Type exception)
+        public void AddNewNullEntityExeption(string repositoryName, Type exception)
         {
             XMLService service = new XMLService(repositoryName);
             Assert.Throws(exception, () => service.AddEntity(null));
